c73ac01ac1e002843a894a10f628f8953082c807
LucSedirae <jondeavers@gmail.com>
2020-11-12 15:54:16 -0500

    Added handlebars index and half of controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,22 @@ const router = express.Router();
 
 const burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        //*hbsObj stands for Handlebars Object
+        let hbsObj = {
+            burgers: data
+        };
+        console.log(hbsObj);
+        res.render("index", hbsObj);
+    })
+});
 
+router.post("api/burgers", function(req, res) {
+    burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function(res) {
+        res.json({id: res.insertId})
+    })
+})
 
 
 
