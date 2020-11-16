e90a41bb7312ff42120d9772cbe0456b47f0b8ce
LucSedirae <jondeavers@gmail.com>
2020-11-16 00:50:02 -0500

    Heroku config

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,22 @@
 //*Dependencies
 const mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "burgers_db"    
+    });
+};
+
+
+
+
+
 //*Sets up MySQL connection
 const connection = mysql.createConnection({
     host: "localhost",
